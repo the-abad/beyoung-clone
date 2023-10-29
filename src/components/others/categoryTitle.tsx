6bfad30ab54cad7addf25150d4e18e5733d13995
Zeeshan Siddiqui <abadzeeshan002@gmail.com>
2023-10-29 15:10:42 +0530

    super changes

--- src/components/others/categoryTitle.tsx
+++ src/components/others/categoryTitle.tsx
@@ -1,14 +1,21 @@
-import './style.css'
+import React from "react";
+import './style.css';
 
-export default function CategoryTitle(props){
- return(
- <>
-  
-  <a name={props.title}></a>
-  <div className="headingbb wrapper">
-    <span className="headingname">{props.title}</span>
-    
-  </div>
- </>
- )
-}
+// Define a type for the component props
+type CategoryTitleProps = {
+  title: string;
+};
+
+// Use the defined type for the props
+const CategoryTitle: React.FC<CategoryTitleProps> = (props) => {
+  return (
+    <>
+      <a data-name={props.title}></a>
+      <div className="headingbb wrapper">
+        <span className="headingname">{props.title}</span>
+      </div>
+    </>
+  );
+};
+
+export default CategoryTitle;
