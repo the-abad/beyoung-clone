2a6981b9fc0601f4ba59d8ae7e396968b046afc3
Zeeshan Siddiqui <abadzeeshan002@gmail.com>
2023-10-29 15:56:14 +0530

    super changes

--- src/components/Authentication/LogIn.js
+++ src/components/Authentication/LogIn.js
@@ -1,69 +1,74 @@
-'use client'
-import { useState } from 'react'
-import './style.css'
-import Image from 'next/image'
+import { useState } from 'react';
+import './style.css';
+import Image from 'next/image';
 import axios from "axios";
 
 export default function LogIn(props) {
+    console.log(props);
 
-	console.log(props)
     const ModalClose = () => {
         props.setShowModal(false);
         props.setShowModal2(true);
-
     }
 
     const ModalClose3 = () => {
         props.setShowModal2(false);
         props.setShowModal(false);
+    }
 
-    }
-    const [form, setForm] = useState();
+    const [form, setForm] = useState({ email: '', password: '' });
     const [isLogin, setIsLogin] = useState(false);
-    const onChange = (e) =>{
-        const name = e.target.name
-        const value = e.target.value
-        setForm({...form, [name]:value});
+
+    const onChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setForm({ ...form, [name]: value });
     }
+
     const submitForm = async (e) => {
         e.preventDefault();
-    
         console.log(form);
 
         const apiUrl = 'https://academics.newtonschool.co/api/v1/user/login';
-    
-        const body = {...form, appType: 'ecommerce'}
+
+        const body = { ...form, appType: 'ecommerce' };
         const headers = {
             'Content-Type': 'application/json',
             'projectID': 's412etnzxy4q',
         };
-    
+
         try {
             const response = await axios.post(apiUrl, body, { headers: headers });
             console.log(response);
+
             if (response?.data?.status === 'success') {
-              const token = response?.data?.token;
-              if (typeof window !== 'undefined') {
-              localStorage.setItem('token', token);}
-              const profile = response?.data?.data;
-              if (typeof window !== 'undefined') {
-              localStorage.setItem('profile', JSON.stringify(profile));}
-              console.log(profile);
-              props.setShowModal(false);
-            //   setIsSigned(true);
+                const token = response?.data?.token;
+                
+                if (typeof window !== 'undefined') {
+                    localStorage.setItem('token', token);
+                }
+
+                const profile = response?.data?.data;
+                
+                if (typeof window !== 'undefined') {
+                    localStorage.setItem('profile', JSON.stringify(profile));
+                }
+
+                console.log(profile);
+                props.setShowModal(false);
+                //   setIsSigned(true);
             }
-          } catch (error) {
+        } catch (error) {
             console.error("Error fetching data: ", error);
-          }
+        }
     }
 
-	return (
-        <> 
-            <div className='LogIn-popUp' style={{ display: props.showModal ? 'block' : 'none' }}>
+    return (
+        <div className='LogIn-popUp' style={{ display: props.showModal ? 'block' : 'none' }}>
             <div className="ReactModal__Overlay ReactModal__Overlay--after-open" style={{ position: 'fixed', inset: '0px', backgroundColor: 'rgba(0, 0, 0, 0.8)', zIndex: 999 }}>
                 <div className="ReactModal__Content ReactModal__Content--after-open login-form-box" tabIndex="-1" role="dialog" aria-modal="true" style={{ top: '0%', overflowY: 'auto', maxHeight: 'calc(100vh - 50px)', outline: 'none' }}>
                     <div className="social-login-title guest-close">
-                        <button onClick={ModalClose3}  title="Close (Esc)" type="button" className="mfp-close" style={{ fontSize: '18px' , cursor:'pointer'}}>
+                        <button onClick={ModalClose3} title="Close (Esc)" type="button" className="mfp-close" style={{ fontSize: '18px', cursor: 'pointer' }}>
                             X
                         </button>
                     </div>
@@ -80,9 +85,8 @@ export default function LogIn(props) {
                             </div>
                             <form method="POST">
                                 <div className="form-in">
-                                    <input type="email" onChange={onChange} name='email' placeholder='Enter your email *' className="" autoComplete="off"   />   
-                                    <input type="text" onChange={onChange} name='password' placeholder='Enter your password *' className="" autoComplete="off" minLength="8"  style={{'margin-top':'10px'}}/>                           
-
+                                    <input type="email" onChange={onChange} name='email' placeholder='Enter your email *' className="" autoComplete="off" />
+                                    <input type="text" onChange={onChange} name='password' placeholder='Enter your password *' className="" autoComplete="off" minLength="8" style={{ 'margin-top': '10px' }} />
                                 </div>
 
                                 <div className="login-forget-password-button">
@@ -93,29 +97,10 @@ export default function LogIn(props) {
                             <a onClick={ModalClose} className="create-account" style={{ display: 'block', textAlign: 'center', marginBottom: '30px' }}>
                                 New to Beyoung? Sign Up
                             </a>
-
                         </div>
                     </div>
-
-
-
-
-
-
                 </div>
             </div>
-
-            </div>
-
-            
-
-            
-            
-        
-        
-        
-        </>
-
-    )
-
+        </div>
+    );
 }
